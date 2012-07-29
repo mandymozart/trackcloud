a171707539e3e3e6273814f8f05cc7ffdd956212
porschuetz <tilman@sinnfrei.co.uk>
2012-07-29 08:58:49 +0200

    fixed root offset

--- application/helper.upload.json.php
+++ application/helper.upload.json.php
@@ -18,7 +18,7 @@ header("Pragma: no-cache");
 
 // Settings
 // $targetDir = ini_get("upload_tmp_dir") . DIRECTORY_SEPARATOR . "plupload";
-$targetDir = '../../root/temp';
+$targetDir = '../root/temp';
 
 $cleanupTargetDir = true; // Remove old files
 $maxFileAge = 5 * 3600; // Temp file age in seconds
